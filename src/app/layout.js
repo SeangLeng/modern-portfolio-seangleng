dd5ccd2d33eefd9427cf35808f9880b8b8f33512
seangleng <seangleng507@gmail.com>
2023-08-17 19:20:08 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Navbar from '@/app/component/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
-import thumbnail from '../../public/images/Programming-amico.png'
+import thumbnail from '../../public/images/thmbnail.png'
 import Loading from './loading'
 import { Suspense } from 'react'
 import Head from 'next/head'
@@ -11,9 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Seng seang leng Portfolio',
   description: 'A web developer is a skilled professional who specializes in creating. i am proficient in various programming languages such as HTML, CSS, and JavaScript, and they utilize their expertise to design and develop visually appealing and user-friendly websites.',
-  thumbnail: "https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468",
+  thumbnail: { thumbnail },
   openGraph: {
-    images: ["https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468"],
+    images: [{ thumbnail }],
   },
 }
 
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
-        <meta name="thumbnail:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
+        <meta property="og:image" content={thumbnail} />
+        <meta name="thumbnail:image" content={thumbnail} />
       </Head>
       <body className={inter.className}>
         <Suspense fallback={<Loading />} >
@@ -31,6 +31,6 @@ export default function RootLayout({ children }) {
         </Suspense>
         <Script src="../path/to/flowbite/dist/flowbite.min.js"></Script>
       </body>
-    </html>
+    </html >
   )
 }
