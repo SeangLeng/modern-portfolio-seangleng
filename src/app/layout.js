49a426f33ddee22f263854826e04657abd0f7486
seangleng <seangleng507@gmail.com>
2023-08-17 18:44:31 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Script from 'next/script'
 import thumbnail from '../../public/images/Programming-amico.png'
 import Loading from './loading'
 import { Suspense } from 'react'
+import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -19,6 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
+        <meta name="frontend:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
+      </Head>
       <body className={inter.className}>
         <Suspense fallback={<Loading />} >
           <Navbar />
