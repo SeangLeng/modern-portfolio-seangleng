d082d75fadb2e7a71bbb0715c8aef17416afe49e
seangleng <seangleng507@gmail.com>
2023-08-17 18:53:14 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,10 +11,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Seng seang leng Portfolio',
   description: 'A web developer is a skilled professional who specializes in creating. i am proficient in various programming languages such as HTML, CSS, and JavaScript, and they utilize their expertise to design and develop visually appealing and user-friendly websites.',
-  thumbnail: "https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468",
-  openGraph: {
-    images: ["https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468"],
-  },
 }
 
 export default function RootLayout({ children }) {
@@ -22,7 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta property="og:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
-        <meta name="frontend:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
+        <meta name="thumbnail:image" content="https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468" />
       </Head>
       <body className={inter.className}>
         <Suspense fallback={<Loading />} >
