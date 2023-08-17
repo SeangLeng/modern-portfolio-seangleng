c237dea94595065d9ce3bee6b6faca32c678744e
seangleng <seangleng507@gmail.com>
2023-08-17 18:55:28 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Seng seang leng Portfolio',
   description: 'A web developer is a skilled professional who specializes in creating. i am proficient in various programming languages such as HTML, CSS, and JavaScript, and they utilize their expertise to design and develop visually appealing and user-friendly websites.',
+  thumbnail: "https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468",
+  openGraph: {
+    images: ["https://elements-cover-images-0.imgix.net/bc1b4639-46ae-441e-987e-cda7482a798c?auto=compress%2Cformat&fit=max&w=900&s=2e76147eb82a97a64ebf419c6a51c468"],
+  },
 }
 
 export default function RootLayout({ children }) {
