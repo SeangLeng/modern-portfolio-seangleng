81f3b2f7d18b7d059b3f38c91a21c3829f84669d
seangleng <seangleng507@gmail.com>
2023-08-17 19:24:33 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Seng seang leng Portfolio',
   description: 'A web developer is a skilled professional who specializes in creating. i am proficient in various programming languages such as HTML, CSS, and JavaScript, and they utilize their expertise to design and develop visually appealing and user-friendly websites.',
-  thumbnail: { thumbnail },
+  thumbnail: "https://scontent.fpnh24-1.fna.fbcdn.net/v/t39.30808-6/366726617_262088796684069_5563290624936517674_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHkMUUOntuKyYJzoREdIG6jaf99vNJFbMdp_3280kVsx9Ot0zyLOdGnIpAc_iq1wYLYYwJMJHiWnUGdAB3_IwEV&_nc_ohc=KVphs4hWEOMAX-3orUR&_nc_ht=scontent.fpnh24-1.fna&oh=00_AfBD1Sci08se2kn6ZxOcGDORBTyV8cf-ZAvznWKi4Cp39w&oe=64E2B4C4",
   openGraph: {
-    images: [{ thumbnail }],
+    images: ["https://scontent.fpnh24-1.fna.fbcdn.net/v/t39.30808-6/366726617_262088796684069_5563290624936517674_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHkMUUOntuKyYJzoREdIG6jaf99vNJFbMdp_3280kVsx9Ot0zyLOdGnIpAc_iq1wYLYYwJMJHiWnUGdAB3_IwEV&_nc_ohc=KVphs4hWEOMAX-3orUR&_nc_ht=scontent.fpnh24-1.fna&oh=00_AfBD1Sci08se2kn6ZxOcGDORBTyV8cf-ZAvznWKi4Cp39w&oe=64E2B4C4"],
   },
 }
 
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:image" content={thumbnail} />
-        <meta name="thumbnail:image" content={thumbnail} />
+        <meta property="og:image" content="https://scontent.fpnh24-1.fna.fbcdn.net/v/t39.30808-6/366726617_262088796684069_5563290624936517674_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHkMUUOntuKyYJzoREdIG6jaf99vNJFbMdp_3280kVsx9Ot0zyLOdGnIpAc_iq1wYLYYwJMJHiWnUGdAB3_IwEV&_nc_ohc=KVphs4hWEOMAX-3orUR&_nc_ht=scontent.fpnh24-1.fna&oh=00_AfBD1Sci08se2kn6ZxOcGDORBTyV8cf-ZAvznWKi4Cp39w&oe=64E2B4C4" />
+        <meta name="thumbnail:image" content="https://scontent.fpnh24-1.fna.fbcdn.net/v/t39.30808-6/366726617_262088796684069_5563290624936517674_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHkMUUOntuKyYJzoREdIG6jaf99vNJFbMdp_3280kVsx9Ot0zyLOdGnIpAc_iq1wYLYYwJMJHiWnUGdAB3_IwEV&_nc_ohc=KVphs4hWEOMAX-3orUR&_nc_ht=scontent.fpnh24-1.fna&oh=00_AfBD1Sci08se2kn6ZxOcGDORBTyV8cf-ZAvznWKi4Cp39w&oe=64E2B4C4" />
       </Head>
       <body className={inter.className}>
         <Suspense fallback={<Loading />} >
@@ -31,6 +31,6 @@ export default function RootLayout({ children }) {
         </Suspense>
         <Script src="../path/to/flowbite/dist/flowbite.min.js"></Script>
       </body>
-    </html >
+    </html>
   )
 }
